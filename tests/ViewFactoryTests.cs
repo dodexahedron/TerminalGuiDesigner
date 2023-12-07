7948eb01e5253752900efed7770a1252fa3fd699
Brandon Thetford <github@snapsinazfs.com>
2023-12-06 21:57:21 -0700

    Add change control test for DefaultMenuBarItems
    Also fix a typo

--- tests/ViewFactoryTests.cs
+++ tests/ViewFactoryTests.cs
@@ -9,6 +9,13 @@ namespace UnitTests;
 [Category( "Core" )]
 internal class ViewFactoryTests : Tests
 {
+    private static readonly MenuBarItem[] DefaultMenuBarItems_ExpectedCollection =
+    {
+        new(
+            "_File (F9)",
+            new[] { new MenuItem( ViewFactory.DefaultMenuItemText, string.Empty, ( ) => { } ) } )
+    };
+
     private static readonly Type[] KnownUnsupportedTypes_ExpectedTypes =
     {
         typeof( Toplevel ),
@@ -24,7 +31,24 @@ internal class ViewFactoryTests : Tests
         typeof( WizardStep )
     };
 
-    private static Type[] ViewFactory_KnownUnsupportdTypes => ViewFactory.KnownUnsupportedTypes;
+    private static MenuBarItem[] ViewFactory_DefaultMenuBarItems => ViewFactory.DefaultMenuBarItems;
+
+    private static Type[] ViewFactory_KnownUnsupportedTypes => ViewFactory.KnownUnsupportedTypes;
+
+    [Test]
+    [Category( "Change Control" )]
+    public void DefaultMenuBarItems_IsExactlyAsExpected( )
+    {
+        Assert.Multiple( ( ) =>
+        {
+            Assert.That( ViewFactory_DefaultMenuBarItems, Has.Length.EqualTo( 1 ) );
+            Assert.That( ViewFactory_DefaultMenuBarItems[ 0 ].Title, Is.EqualTo( "_File (F9)" ) );
+        } );
+
+        Assert.That( ViewFactory_DefaultMenuBarItems[ 0 ].Children, Has.Length.EqualTo( 1 ) );
+        Assert.That( ViewFactory_DefaultMenuBarItems[ 0 ].Children[ 0 ].Title, Is.EqualTo( ViewFactory.DefaultMenuItemText ) );
+        Assert.That( ViewFactory_DefaultMenuBarItems[ 0 ].Children[ 0 ].Help, Is.Empty );
+    }
 
     [Test]
     [Description( "Checks that all tested types exist in the collection in ViewFactory" )]
@@ -37,8 +61,15 @@ internal class ViewFactoryTests : Tests
     [Test]
     [Description( "Checks that no new types have been added that aren't tested" )]
     [Category( "Change Control" )]
-    public void KnownUnsupportedTypes_DoesNotContainUnexpectedItems( [ValueSource( nameof( ViewFactory_KnownUnsupportdTypes ) )] Type typeDeclaredUnsupportedInViewFactory )
+    public void KnownUnsupportedTypes_DoesNotContainUnexpectedItems( [ValueSource( nameof( ViewFactory_KnownUnsupportedTypes ) )] Type typeDeclaredUnsupportedInViewFactory )
     {
         Assert.That( KnownUnsupportedTypes_ExpectedTypes, Contains.Item( typeDeclaredUnsupportedInViewFactory ) );
+    }
+
+    [Test]
+    [Category( "Change Control" )]
+    public void ViewType_IsTerminalGuiView( )
+    {
+        Assert.That( ViewFactory.ViewType, Is.EqualTo( typeof( View ) ) );
     }
 }
